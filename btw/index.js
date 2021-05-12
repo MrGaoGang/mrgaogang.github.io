44380d2797162b59a88cb4cae0ca607a820eea74
MrGaoGang <gaogangwork@qq.com>
2021-05-12 13:06:55 +0000

    Update from https://github.com/MrGaoGang/lucky_docs/commit/f0a977deac2afb815ff78b72931ff926f99070bd)

--- btw/index.js
+++ btw/index.js
@@ -1,32 +1,13 @@
-// window.onload = function() {
-//   var themeDefaultContent = $(
-//     "#app > .theme-container>.page > .theme-default-content"
-//   );
-//   themeDefaultContent.attr("id", "container");
-//   btw = new BTWPlugin(); // 注意btw需要是个全局变量,把const去掉
-//   btw.init({
-//     id: "container",
-//     blogId: "26610-1620818470013-209",
-//     name: "高小哥前端开发",
-//     qrcode:
-//       "https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/4959d28b5b64456a8148cc9d312a05dd~tplv-k3u1fbpfcp-watermark.image",
-//     keyword: "验证码",
-//   });
-//   // window.localStorage.setItem('mrgaogang-blog-has-focus',1)
-// };
-
-module.exports = function() {
+window.onload = function() {
   var css = document.createElement("style");
   css.innerHTML = `
   #read-more-wrap{
 
-    position: fixed !important;
     top: 0px;
     left: 0px;
     bottom: 0px;
     z-index: 9999;
     width: 100%;
-    height: 100%;
     font-family: PingFangSC-Regular, sans-serif;
 
 }
@@ -59,6 +40,11 @@ module.exports = function() {
 
   setTimeout(()=>{
     var more = document.getElementById('read-more-btn');
-    more.innerText = '点击阅读全文'
+    more.innerText = '点击阅读全文';
+
   },2000)
+};
+
+module.exports = function() {
+  
 };
