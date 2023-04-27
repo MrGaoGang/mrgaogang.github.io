81baa6da5a7f8f907127f947db0bad1ee6caa1fe
mrgaogang <gaogangwork@qq.com>
2023-04-27 07:38:56 +0000

    feat:update docs

--- btw/index.js
+++ btw/index.js
@@ -32,7 +32,7 @@ window.onload = function() {
   btw.init({
     id: "container",
     blogId: "26610-1620818470013-209",
-    name: "高小哥前端开发",
+    name: "高小哥漫谈前端",
     qrcode:
       "https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/4959d28b5b64456a8148cc9d312a05dd~tplv-k3u1fbpfcp-watermark.image",
     keyword: "验证码",
